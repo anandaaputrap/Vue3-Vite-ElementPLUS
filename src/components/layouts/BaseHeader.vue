af49b51b21871b3ae4a0309f49d68e64e8dc3b98
anandaaputrap <anandapratama4869@gmail.com>
2023-06-26 10:44:40 +0700

    update

--- src/components/layouts/BaseHeader.vue
+++ src/components/layouts/BaseHeader.vue
@@ -6,19 +6,15 @@ import { toggleDark } from "~/composables";
   <el-menu class="el-menu-demo" mode="horizontal">
     <el-menu-item index="1">Logo Disini</el-menu-item>
     <el-sub-menu index="2">
-      <template #title>Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-sub-menu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-sub-menu>
+      <template #title>Menu 1</template>
+      <el-menu-item index="2-1">List Data</el-menu-item>
+      <el-menu-item index="2-2">Tambah Data</el-menu-item>
     </el-sub-menu>
-    <el-menu-item index="3" disabled>Info</el-menu-item>
-    <el-menu-item index="4">Orders</el-menu-item>
+    <el-sub-menu index="3">
+      <template #title>Menu 2</template>
+      <el-menu-item index="3-1">List Data</el-menu-item>
+      <el-menu-item index="3-2">Tambah Data</el-menu-item>
+    </el-sub-menu>
     <el-menu-item h="full" @click="toggleDark()">
       <button
         class="border-none w-full bg-transparent cursor-pointer"
@@ -29,3 +25,7 @@ import { toggleDark } from "~/composables";
     </el-menu-item>
   </el-menu>
 </template>
+
+<style scoped>
+
+</style>
